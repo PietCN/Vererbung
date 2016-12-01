4df9b79168b2c3e596c855699774aa195fed47c0
Patrick <Patrick@ZENBOOK>
2016-12-01 14:44:53 +0100

    Figur implementiert

---
+++ Figur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public abstract class Figur
+{
+  //Felder
+  string Farbe;
+
+  public Figur(string Farbe)
+  {
+    this.Farbe = Farbe;
+  }
+
+  public abstract void Show();
+}
+
