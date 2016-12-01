fd3ff5d9be7cb2d4fb74d66d49ddc892dac70725
Patrick <Patrick@ZENBOOK>
2016-12-01 15:10:24 +0100

    Parallelogram implementiert, Ellipse wird nicht mehr mit eingefügt. Tests durchgeführt

---
+++ Parallelogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Parallelogram : Rechteck
+{
+  protected double winkel;
+
+  public Parallelogram(string farbe, double laenge, double breite, double winkel) : base(farbe, laenge, breite)
+  {
+    if (winkel > 0 && winkel <= 90)
+      this.winkel = winkel;
+    else
+      this.winkel = 90; //Rechteck
+  }
+
+  public override void Show()
+  {
+    Console.WriteLine("Parallelogram-Objekt");
+    Console.WriteLine("Farbe: " + Farbe);
+    Console.WriteLine("Größe: " + breite + " x " + laenge + "  Winkel: " + winkel + "°");
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ class Program
     Kreis kreis = new Kreis("blau", 10.0);
     Rechteck rechteck = new Rechteck("grün", 5.0, 2.3);
     Ring ring = new Ring("lila", 30.0, 15.0);
+    Parallelogram para = new Parallelogram("rot", 40.0, 20.0, 30.0);
 
     kreis.Show();
     rechteck.Show();
     ring.Show();
+    para.Show();
 
     Console.ReadKey();
   }
