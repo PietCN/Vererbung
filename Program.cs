3855064055174ad5f70e78fa1d7f6e649df3f0eb
Patrick <Patrick@ZENBOOK>
2016-12-01 15:18:05 +0100

    Array vom Typ Figur erstellt und mehrere Instanzen von den Ableitungen eingetragen. Anschließend von allen die Methode Show() ausgeführt

--- Program.cs
+++ Program.cs
@@ -4,15 +4,22 @@ class Program
 {
   static void Main()
   {
-    Kreis kreis = new Kreis("blau", 10.0);
-    Rechteck rechteck = new Rechteck("grün", 5.0, 2.3);
-    Ring ring = new Ring("lila", 30.0, 15.0);
-    Parallelogram para = new Parallelogram("rot", 40.0, 20.0, 30.0);
+    Figur[] figuren = new Figur[10];
 
-    kreis.Show();
-    rechteck.Show();
-    ring.Show();
-    para.Show();
+    figuren[0] = new Kreis("blau", 3.0);
+    figuren[1] = new Kreis("rot", 7.3);
+    figuren[2] = new Rechteck("lila", 20.5, 30.7);
+    figuren[3] = new Ring("blau", 20.9, 19.3);
+    figuren[4] = new Rechteck("grün", 10.2, 4.9);
+    figuren[5] = new Parallelogram("grau", 70.0, 20.4, 67.0);
+    figuren[6] = new Ring("magenta", 20.0, 17.2);
+    figuren[7] = new Rechteck("schwarz", 90.3, 73.5);
+    figuren[8] = new Kreis("gelb", 34.4);
+    figuren[9] = new Parallelogram("blau", 44.2, 19.9, 22.6);
+
+
+    foreach (Figur fig in figuren)
+      fig.Show();
 
     Console.ReadKey();
   }
